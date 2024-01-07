bf1e43698aa2865cffaad84be9385c13a4c03ac3
Prakash Niroula <prakashpradipbrothers@gmail.com>
2024-01-07 15:28:37 +0545

    SVG WORKS NOW

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
+import Spotify from './assets/login/svgs/spotify.svg';
+
 export default function App() {
   return (
     <View className="bg-red-500 flex h-full justify-center items-center">
       <Text>Open up App to start working on your app!</Text>
+      <Spotify />
       <StatusBar style="auto" />
     </View>
   );
