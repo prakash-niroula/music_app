5789c4596b32254297235c81ac7b8f114113b01c
Prakash Niroula <prakashpradipbrothers@gmail.com>
2024-01-08 13:03:02 +0545

    0.01% progress

--- App.tsx
+++ App.tsx
@@ -1,14 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
+
 import { Text, View } from 'react-native';
 
+import {
+  useFonts, Inter_100Thin, Inter_200ExtraLight, Inter_300Light,
+  Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_700Bold,
+  Inter_800ExtraBold, Inter_900Black
+} from '@expo-google-fonts/inter';
+
 import Spotify from './assets/login/svgs/spotify.svg';
+import { useState } from 'react';
+import Default from './components/Default/Default';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_100Thin, Inter_200ExtraLight, Inter_300Light,
+    Inter_400Regular, Inter_500Medium, Inter_600SemiBold,
+    Inter_700Bold, Inter_800ExtraBold, Inter_900Black
+  })
+  
+  if ( !fontsLoaded ) {
+    return <></>
+  }
+
   return (
-    <View className="bg-red-500 flex h-full justify-center items-center">
-      <Text>Open up App to start working on your app!</Text>
-      <Spotify />
-      <StatusBar style="auto" />
-    </View>
+    <Default />
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,29 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  theme: {
+    extend: {
+      colors: {
+        'main-black': '#272727',
+        'main-grey': '#585858',
+        'main-blue': '#5184FE',
+        'rectangle-grey': '#F8F8F8',
+      },
+      fontFamily: {
+        'interThin': ['Inter_100Thin'],
+        'interExtraLight': ['Inter_200ExtraLight'],
+        'interLight': ['Inter_300Light'],
+        'interRegular': ['Inter_400Regular'],
+        'interMedium': ['Inter_500Medium'],
+        'interSemiBold': ['Inter_600SemiBold'],
+        'interBold': ['Inter_700Bold'],
+        'interExtraBold': ['Inter_800ExtraBold'],
+        'interBlack': ['Inter_900Black'],
+      },
+      letterSpacing: {
+        'def': -2.5,
+      }
+    }
+  }
   // ...
 };
