206b4c7adb1776aeeb518745942ae5d911ac29ba
Prakash Niroula <prakashpradipbrothers@gmail.com>
2024-01-08 13:03:32 +0545

    0.01% progress

---
+++ components/Default/Default.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import LoginScreen from "../LoginScreen/LoginScreen";
+
+const myStyle = StyleSheet.create({
+  '*': {
+    letterSpacing: -20
+  }
+})
+
+export const Default: FC = () => {
+  return (
+    <View className="flex justify-center items-center h-full">
+      <LoginScreen />
+    </View>
+  );
+}
+
+export default Default
